02a8177c790a034c6816c158cfabf5a92f93aff7
chitraabanu <chitraabanues@gmail.com>
2025-04-26 14:54:15 +0100

    first commit

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter:
+  //[['allure-playwright',{outputFolder:'my-allure-reports'}]],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,7 +33,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'off',
-    video:'on'
+    
   },
 
   /* Configure projects for major browsers */
